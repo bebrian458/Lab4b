50e7eb6b964bf011579676e85b1ef6522ed05dca
Brian Be <bebrian458@gmail.com>
2017-05-19 16:17:36 -0700

    Uploading files to transfer to Edison

---
+++ Makefile
@@ -0,0 +1,13 @@
+# NAME:	Brian Be
+# EMAIL:	bebrian458@gmail.com
+# ID: 	204612203
+
+
+default: lab4b
+
+lab4b:
+
+check:
+
+dist:
+
---
+++ lab4b.c
@@ -0,0 +1,11 @@
+// NAME:	Brian Be
+// EMAIL:	bebrian458@gmail.com
+// ID: 	204612203
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+int main(int argc, char *argv[]){
+	
+}
